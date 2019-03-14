7779577d9a4454a776c773db0eccca0ac5f4394e
KewenC <KewenC@outlook.com>
2019-03-14 23:38:34 +0800

    更新

--- app/src/main/java/com/example/songt/bluedemo/MainActivity.java
+++ app/src/main/java/com/example/songt/bluedemo/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.songt.bluedemo;
 
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.inuker.bluetooth.library.BluetoothClient;
@@ -32,18 +37,37 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> nameList;
     private ArrayList<String> macList;
     private String mac;
+    private ProgressBar progressBar;
+    private BluetoothAdapter bluetoothAdapter;
+    private int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
 
         nameList = new ArrayList<>();
         macList = new ArrayList<>();
 
         mClient = new BluetoothClient(this);
 
-        scan();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter != null) {
+            if (!bluetoothAdapter.isEnabled()) {
+                //开启蓝牙
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
+            } else {
+                scan();
+                Toast.makeText(this,"蓝牙已打开！",Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this,"此设备不支持蓝牙！",Toast.LENGTH_SHORT).show();
+            Log.e("TAGF", "此设备不支持蓝牙！");
+        }
+
+
     }
 
     private void scan() {
@@ -61,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
 
             public void onSearchStarted() {//开始搜素
+                progressBar.setVisibility(View.VISIBLE);
                 Log.e("TAGF","onSearchStarted");
             }
 
@@ -80,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
 
             public void onSearchStopped() {//搜索停止
+                progressBar.setVisibility(View.GONE);
                 Log.e("TAGF","onSearchStopped");
                 if (nameList.size() <= 0) {
                     Toast.makeText(MainActivity.this,"没发现可用设备！",Toast.LENGTH_LONG).show();
@@ -96,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                progressBar.setVisibility(View.VISIBLE);
                                 connect(macList.get(which));
                             }
                         });
@@ -105,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
 
             public void onSearchCanceled() {//搜索取消
+                progressBar.setVisibility(View.GONE);
                 Log.e("TAGF","onSearchCanceled");
             }
 
@@ -130,7 +158,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
 
             public void onResponse(int code, BleGattProfile data) {
-                Log.e("TAGF","onResponse");
+                Log.e("TAGF","onResponse_code="+code+"_data="+data.toString());
+
             }
 
         });
@@ -159,6 +188,21 @@ public class MainActivity extends AppCompatActivity {
         if (mClient != null && !mac.equals("")){
             mClient.unregisterConnectStatusListener(mac, mBleConnectStatusListener);
             mClient.disconnect(mac);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT){
+            if (!bluetoothAdapter.isEnabled()) {
+                Toast.makeText(this,"蓝牙打开失败！",Toast.LENGTH_SHORT).show();
+                Log.e("TAGF", "蓝牙打开失败！");
+            } else {
+                scan();
+                Toast.makeText(this,"蓝牙打开成功！",Toast.LENGTH_SHORT).show();
+                Log.e("TAGF", "蓝牙打开成功！");
+            }
         }
     }
 }
